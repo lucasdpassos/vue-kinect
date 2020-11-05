1572dd31fe41862eca8823e87cd5dd9643e9c519
Lucas Passos <lucas.depassos@yahoo.com.br>
2020-11-04 22:13:21 -0300

    adjustements

--- src/App.vue
+++ src/App.vue
@@ -1,35 +1,33 @@
 <template>
-  <v-app>  
-    <v-img src="../src/assets/codepacklogo.png" class="logo1" style="max-width:50px;" />
+<v-app>
+    <v-img src="../src/assets/codepacklogo2.png" class="logo1" style="width:50px;height:20px;" />
     <v-main>
-      <Card1 />
+        <Card1 />
     </v-main>
-  </v-app>
+    <Footer />
+</v-app>
 </template>
 
 <script>
 import Card1 from './components/Card1'
-
-
+import Footer from './components/Footer'
 
 export default {
-  name: 'App',
+    name: 'App',
 
-  components: {
-    Card1,
-  },
+    components: {
+        Card1,
+        Footer
+    },
 
-  data: () => ({
-    //
-  }),
+    data: () => ({
+        //
+    }),
 };
 </script>
 
-
 <style scoped>
-
 .logo1 {
-  max-width: 400px;
+    max-width: 400px;
 }
-
 </style>
---
+++ src/components/Footer.vue
@@ -0,0 +1,11 @@
+<template>
+<v-footer color="red accent-3" dark>
+    Code Pack 2020 ©
+</v-footer>
+</template>
+
+<script>
+export default {
+    //
+}
+</script>
